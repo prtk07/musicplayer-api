30639b019f4f7c465a12062395268c1ef56ae61b
Prateek Verma <prateekverma70@gmail.com>
2021-08-30 19:50:46 +0530

    feat: cors added, error message handling added

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const cors = require("cors");
 const registerAPI = require("./routes");
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log("error at connection" + e);
+  });
 
 const app = express()
   .use(bodyparser.json())
-  .use(bodyparser.urlencoded({ extended: true }));
+  .use(bodyparser.urlencoded({ extended: true }))
+  .use(cors());
 
 registerAPI(app);
 
--- routes.js
+++ routes.js
@@ -4,4 +4,17 @@ const user = require("./api/users");
 module.exports = (app) => {
   app.get("/", (req, res, next) => res.sendStatus(200));
   app.use("/user", user, responseHandler);
+
+  app.use((err, req, res, next) => {
+    if (err) {
+      const status =
+        res.locals.status ||
+        err.statusCode ||
+        err.status ||
+        (err.output && err.output.statusCode) ||
+        400;
+      return res.status(status).send({ message: err.message });
+    }
+    return next();
+  });
 };
